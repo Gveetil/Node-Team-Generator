8028f11ac05eb3c5b32f5dee633d1085ea19a6b8
Geethanjali Veetil <gtveetil@gmail.com>
2020-04-16 16:09:25 +1000

    updated application functionality

--- app.js
+++ app.js
@@ -1,28 +1,99 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const render = require("./lib/htmlRenderer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const htmlRenderer = require("./lib/htmlRenderer");
+const Questions = require("./lib/Questions");
 
+/** output path where file is generated and saved */
 const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPathHTML = path.join(OUTPUT_DIR, "team.html");
+const outputPathCSS = path.join(OUTPUT_DIR, "style.css");
 
+/** Header message displayed to the user on execution */
+const userMessage = "This utility will walk you through creating an Information page for your team.\n\nPress ^C at any time to quit.\n\nPlease enter the below details ...";
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+/**
+ * Saves the HTML Team Information to new file(s) 
+ * @param {*} teamHtml the HTML Team Information 
+ * @param {*} teamCSS the HTML Team Information stylesheet 
+ */
+function createTeamHtmlFile(teamHtml, teamCSS) {
+    // Create output folder if it doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    // Save HTML and CSS to file
+    fs.writeFileSync(outputPathHTML, teamHtml);
+    fs.writeFileSync(outputPathCSS, teamCSS);
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+/** This method initializes and executes the application */
+async function init() {
+    try {
+        console.log(userMessage);
+        const employees = await fetchEmployees();
+        const teamCSS = htmlRenderer.renderCSS();
+        const teamHTML = htmlRenderer.render(employees);
+        createTeamHtmlFile(teamHTML, teamCSS);
+        console.log(`Team Information file generated successfully at: ${outputPathHTML}`);
+    }
+    catch (error) {
+        console.log(`\n\nTeam Information Generation failed!\nException encountered:\n ${error.stack}`);
+    }
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+/** 
+ * This method prompts the user for team member details, loads the answers 
+ * into an array of employees and returns the same
+ * @returns {Array<Employee>} the array of employees input by the user 
+ */
+async function fetchEmployees() {
+    try {
+        // The first action is always - add a manager
+        var userAction = Questions.ActionTypes.AddManager;
+        var employees = [];
+        // Loop until the user selects the generate option
+        do {
+            var response = await inquirer.prompt(Questions.fetch(userAction));
+            let newEmployee = loadEmployee(userAction, response);
+            employees.push(newEmployee);
+            // Switch to the user's next action 
+            userAction = response.actionType;
+        } while (userAction != Questions.ActionTypes.Generate)
+        return employees;
+    }
+    catch (error) {
+        throw new Error(error.stack);
+    }
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+/**
+ * Create and return an Employee object based on the user's input 
+ * @param {Questions.ActionTypes} actionType the current action type 
+ * @param {object} response the response object containing the user's inputs
+ * @returns {Employee} returns an Employee obect 
+ */
+function loadEmployee(actionType, { name, id, email, officeNumber, school, github }) {
+    const employeeId = parseInt(id);
+    switch (actionType) {
+        case Questions.ActionTypes.AddManager:
+            {
+                const parsedOfficeNumber = parseInt(officeNumber);
+                return new Manager(name, employeeId, email, parsedOfficeNumber);
+            }
+        case Questions.ActionTypes.AddEngineer:
+            return new Engineer(name, employeeId, email, github);
+        case Questions.ActionTypes.AddIntern:
+            return new Intern(name, employeeId, email, school);
+        default:
+            break;
+    }
+    return null;
+}
+
+// Call the init method to start the application
+init();
